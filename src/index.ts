47a20a01c68f8850828d72a8a14c0eb007f17890
Dustin Silk <dustin@staze.com>
2022-10-16 11:19:17 +0100

    Export qwikExchange

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { getClient } from './client/get-client';
 export { UrqlProvider } from './components/urql-provider';
+export { qwikExchange } from './exchange/qwik-exchange';
 export { useMutation } from './hooks/use-mutation';
 export { useMutationResource } from './hooks/use-mutation-resource';
 export { useQuery } from './hooks/use-query';
