1a6fd1af0731a723ac86230a91167877de60a9fb
Dustin J Silk <dustinjsilk@gmail.com>
2022-11-22 17:29:33 +0100

    Add test for nested query data

--- src/hooks/use-query.spec.tsx
+++ src/hooks/use-query.spec.tsx
@@ -14,6 +14,12 @@ export const clientFactory = () => {
     exchanges: [
       executeExchange({
         schema: buildSchema(`
+        type Director {
+          id: String!
+          favouriteFilm: Film!
+          worstFilm: Film!
+        }
+
         type Film {
           id: String!
           title: String!
@@ -21,12 +27,20 @@ export const clientFactory = () => {
 
         type Query {
           film(id: String!): Film
+          director(id: String!): Director
         }
       `),
         rootValue: {
           film: async ({ id }: { id: string }) => {
             return {
               title: 'Title',
+              id,
+            };
+          },
+          director: async ({ id }: { id: string }) => {
+            return {
+              favouriteFilm: { id: '1', title: 'Favourite Film' },
+              worstFilm: { id: '2', title: 'Worst Film' },
               id,
             };
           },
@@ -76,4 +90,60 @@ test(`[useQuery hook]: Should return query data`, async () => {
   );
 
   expect(screen.outerHTML).toContain('Title');
+});
+
+export type DirectorQueryResponse = {
+  director: {
+    id: string;
+    favouriteFilm: { id: string; title: string };
+    worstFilm: { id: string; title: string };
+  };
+};
+
+export const DirectorQuery = gql<DirectorQueryResponse, { id: string }>`
+  query director($id: String!) {
+    director(id: $id) {
+      id
+      favouriteFilm {
+        id
+        title
+      }
+      worstFilm {
+        id
+        title
+      }
+    }
+  }
+`;
+
+test(`[useQuery hook]: Should return nested data`, async () => {
+  const { screen, render } = await createDOM();
+
+  const Child = component$(() => {
+    const variables = useStore({ id: '1' });
+    const query = useQuery(DirectorQuery, variables);
+
+    return (
+      <Resource
+        value={query}
+        onResolved={(res) => {
+          return (
+            <div>
+              {res.data?.director.favouriteFilm.title}
+              {res.data?.director.worstFilm.title}
+            </div>
+          );
+        }}
+      />
+    );
+  });
+
+  await render(
+    <UrqlProvider client={$(clientFactory)}>
+      <Child />
+    </UrqlProvider>
+  );
+
+  expect(screen.outerHTML).toContain('Favourite Film');
+  expect(screen.outerHTML).toContain('Worst Film');
 });
